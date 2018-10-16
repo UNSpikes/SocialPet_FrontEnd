2248d5d6e198ce42edba60442da10eb7467bbdfb
GabrielUnal <gebejaranod@unal.edu.co>
2018-10-15 23:40:41 -0500

    validacion SigIn LogIn

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -3,16 +3,61 @@ import {styles} from './loginStyle';
 import '../../components/login/style.css'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-
 export class LogIn extends React.Component{
 	
 	
-	
+	constructor(props){
+		super(props)
+		this.state = {	user: '',
+						password: '',
+						userError: '',
+						passwordError: ''
+					}
+		this.handleUser = this.handleUser.bind(this);
+		this.handlePassword = this.handlePassword.bind(this);
+		this.submitLogIn = this.submitLogIn.bind(this);
+	}
+
+	handleUser(e){
+		this.setState({user: e.target.value});
+	}
+
+	handlePassword(e){
+		//Must be at least 8 characters
+		//At least 1 special character from @#$%&
+		//At least 1 number, 1 lowercase, 1 uppercase letter
+		this.setState({password: e.target.value})
+	}
+
+
+
+	submitLogIn(e){
+		let isValid = true;
+		if(this.state.user === ''){
+			this.setState({userError: 'Ingrese un usario'});
+			isValid = false;	
+		}else{
+			this.setState({userError: ''});
+		}
+		if(this.state.password === ''){
+			this.setState({passwordError: 'Ingrese la Contraseña'})
+			isValid = false;
+		}else{
+			this.setState({passwordError: ''})
+		}
+		if(isValid){
+			//enviar
+			console.log(this.state)
+		}
+	}
+
+
 	render(){
 		let src = "https://www.anipedia.net/imagenes/taxonomia-perros.jpg";
 		let facebook = 'https://icon-icons.com/icons2/555/PNG/512/facebook_icon-icons.com_53612.png';
 		let google = 'https://elviejodragon.com/wp-content/uploads/2015/01/google__round_logo.png';
 		let twitter = 'https://tarotdecarmenylidia.files.wordpress.com/2016/08/twitter-icon-770x769.png';
+
 		return (
 			<div className="loginBox">
 				<a href={src}><img src={src} className="imagen"/></a>
@@ -28,9 +73,11 @@ export class LogIn extends React.Component{
 						<label >o</label>
 					</div>
 					<div className="o">
-						<input className="text" type="text" placeholder="Usuario"/>
-						<input className="text" type="password" placeholder="Contraseña"/>
-						<button className="button">Sig In</button>
+						<input className="text" type="text" placeholder="Usuario" onChange={this.handleUser}  value={this.state.user}/>
+						<h5 className="alerta">{this.state.userError}</h5>
+						<input className="text" type="password" placeholder="Password" onChange={this.handlePassword} value={this.state.password}/>
+						<h5 className="alerta">{this.state.passwordError}</h5>
+						<button className="button1" onClick={this.submitLogIn} >Sig In</button>
 					</div>
 					<div className="links">
 						<a>Olvidaste tu Contraseña</a>
--- src/components/registrer/registrer.js
+++ src/components/registrer/registrer.js
@@ -4,16 +4,128 @@ import '../../components/registrer/style.css'
 
 export class Registrer extends React.Component{
 
+	constructor(props){
+		super(props)
+		this.state = {
+			fields: {},
+			errors: {},
+			conditions: false,
+			class_c: "checkbox"
+		}
+		this.handleChange = this.handleChange.bind(this)
+		this.submitForm = this.submitForm.bind(this)
+	}
+
+	handleChange(e){
+		let fields = this.state.fields;
+		if(e.target.name.match("condiciones")){
+			let bool = this.state.conditions;
+			fields[e.target.name] = !bool	
+			this.setState({conditions: !bool});
+		}else{
+			fields[e.target.name] = e.target.value;
+		}
+		this.setState({fields});
+		console.log(this.state.fields);
+	}
+
+
+	submitForm(e){
+		//e.preventDefault();
+		if(this.validateForm()){
+			let fields = {}
+			fields["fisrtName"] = "";
+			fields["lastName"] = "";
+			fields["email"] = "";
+			fields["phone"] = "";
+			fields["address"] = "";
+			fields["password"] = "";
+			fields["secondPassword"] = "";
+			// mandar el post
+			//..
+			//..
+		}
+	}
+
+	validateForm(){
+		let fields = this.state.fields;
+		let errors = {}
+		let isValid = true;
+
+		if (!fields["fisrtName"]){
+			isValid=false;
+			errors["fisrtName"] = "Ingrese un Nombre";
+		}else if(!fields["fisrtName"].match(/^[a-zA-Z ]*$/)){
+			isValid = false;
+			errors["fisrtName"] = "Ingrese un Nombre que Contenga Solo Caracteres Alfabeticos";
+		}
+		
+		if (!fields["lastName"]){
+			isValid=false;
+			errors["lastName"] = "Ingrese un Apellido";
+		}else if(!fields["lastName"].match(/^[a-zA-Z ]*$/)){
+			isValid = false;
+			errors["lastName"] = "Ingrese un Apellido que Contenga solo Caracteres Alfabeticos";	
+		}
+
+		if (!fields["email"]){
+			isValid=false;
+			errors["email"] = "Ingrese una Direccion de Correo";
+		}else if(!fields["email"].match(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i)){
+			isValid=false;
+			errors["email"] = "Ingrese una Direccion de Correo Valida";
+		}
+
+		if (!fields["password"]){
+			isValid=false;
+			errors["password"] = "Ingrese una Contraseña";
+		}else if(!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)){
+			isValid = false;
+			errors["password"] = "La Contraseña debe Tener una Mayuscula un Numero y un Caracter Especial";
+		}
+
+		if (!fields["secondPassword"]){
+			isValid=false;
+			errors["secondPassword"] = "Vuelva a Ingresar La Contraseña";
+		}else if(!fields["secondPassword"].match(fields["password"])){
+			isValid=false;
+			errors["secondPassword"] = "Las Contraseñas no Coinciden";
+		}
+
+		if (typeof fields["phone"] !== "undefined"){
+			if(!fields["phone"].match(/^[0-9]{10}$/)){
+				isValid=false;
+				errors["phone"] = "Numero Invalido";
+			}
+		}
+
+		if(!(this.state.conditions)){
+			isValid = false;
+			this.setState({class_c: "alerta3"})
+		}else{
+			this.setState({class_c: "checkbox"})
+		}
+		
+
+		this.setState({errors: errors});
+		return isValid;
+	}
+
+
 	render(){
 		return(
 			<div className="registrerBox">
 				<h1>Registrate</h1>
 				<div className="form">
-					<input type="text" placeholder="First Name" className="text"></input>
-					<input type="text" placeholder="Last Name" className="text"	></input>
-					<input type="text" placeholder="Email" className="text" ></input>
-					<input type="text" placeholder="Phone" className="text" ></input>
-					<input type="text" placeholder="Address" className="text" ></input> 
+					<input type="text" placeholder="First Name" className="text" name="fisrtName" value={this.state.fields.fisrtName} onChange={this.handleChange}></input>
+					<input type="text" placeholder="Last Name" className="text"	name="lastName" value={this.state.fields.lastName} onChange={this.handleChange}></input>
+					<h5 className="alerta2">{this.state.errors.fisrtName}</h5>
+					<h5 className="alerta2">{this.state.errors.lastName}</h5>
+					<input type="text" placeholder="Email" className="text" name="email" value={this.state.fields.email} onChange={this.handleChange}></input>
+					<input type="text" placeholder="Phone" className="text" name="phone" value={this.state.fields.phone} onChange={this.handleChange}></input>
+					<h5 className="alerta2">{this.state.errors.email}</h5>
+					<h5 className="alerta2">{this.state.errors.phone}</h5>
+					<input type="text" placeholder="Address" className="text" name="address" value={this.state.fields.address} onChange={this.handleChange}></input> 
 					<select className="text">
 						<option value="" disabled selected>Select your City</option>
 	  					<option value="Bogota">Bogota</option>
@@ -21,12 +133,16 @@ export class Registrer extends React.Component{
 	  					<option value="Medellin">Medellin</option>
 	  					<option value="Cartagena">Cartagena</option>
 					</select>
-					<input type="Password" placeholder="Password" className="text" ></input>
-					<input type="Password" placeholder="Confirm Password" className="text" ></input>
+					<h5 className="alerta2"></h5>
+					<h5 className="alerta2"></h5>
+					<input type="Password" placeholder="Password" className="text" name="password" value={this.state.fields.password} onChange={this.handleChange}></input>
+					<input type="Password" placeholder="Confirm Password" className="text" name="secondPassword" value={this.state.fields.secondPassword} onChange={this.handleChange}></input>
+					<h5 className="alerta2">{this.state.errors.password}</h5>
+					<h5 className="alerta2">{this.state.errors.secondPassword}</h5>
 				</div>
 				<div className="acept">
-					<label className="checkbox"><input type="checkbox"/>I agree whit the terms and politics of privacity</label>
-					<button className="button">Create Acount</button>
+					<label className={this.state.class_c} name="condiciones" ><input type="checkbox" name="condiciones" onClick={this.handleChange} value={this.state.fields.condiciones}/>I agree whit the terms and politics of privacity</label>
+					<button className="button" onClick={this.submitForm}>Create Acount</button>
 				</div>
 			</div>
 		);
